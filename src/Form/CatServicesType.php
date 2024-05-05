40d56284df151fcadbdee97b63e56542b38a0d92
Oreilleu <aurelien.picardpabijan@gmail.com>
2024-05-05 17:36:53 +0200

    feature/form WIP

--- src/Form/CatServicesType.php
+++ src/Form/CatServicesType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\CatServices;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,11 @@ class CatServicesType extends AbstractType
     {
         $builder
             ->add('nom')
-        ;
+            ->add('filePath', FileType::class, [
+                'label' => 'Choisir un fichier',
+                'mapped' => true,
+                'required' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
