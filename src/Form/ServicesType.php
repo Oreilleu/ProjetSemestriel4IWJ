4f6ee34dab6cc257dc6a3fdd812e1b2c886a4e3b
Salim-T <tizi_salim@yahoo.fr>
2024-05-09 14:26:29 +0200

    feature/ make validation form

--- src/Form/ServicesType.php
+++ src/Form/ServicesType.php
@@ -7,13 +7,18 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+
 class ServicesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nom')
-            ->add('prix')
+            ->add('prix', NumberType::class, [
+                'scale' => 2,
+            ])
             ->add('id_cat_services')
         ;
     }
