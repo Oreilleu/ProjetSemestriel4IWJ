38f6fbf8d13b342162f7bb9f01e85dee61bf68cd
Salim-T <tizi_salim@yahoo.fr>
2024-05-16 19:14:55 +0200

    fix/change tables names services to produits, catservices to categoriesproduits, detailsServices to ligneDevis

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -19,7 +19,9 @@ class ProduitsController extends AbstractController
     {
         return $this->render('produits/index.html.twig', [
             'produits' => $produitsRepository->findAll(),
-        ]);
+        ]);  
+
+        
     }
 
     #[Route('/new', name: 'app_produits_new', methods: ['GET', 'POST'])]
