d0d2f300bca07c8f21f49dfcd6f5b233f37468e3
fatoumata <fatoumatadiaby21@yahoo.com>
2024-06-29 18:34:58 +0200

    dashboard_V2

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,78 +1,60 @@
 <?php
 
 namespace App\Controller;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+
 use App\Repository\ClientsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class DashboardController extends AbstractController
 {
-
     private $clientsRepository;
 
     public function __construct(ClientsRepository $clientsRepository)
     {
         $this->clientsRepository = $clientsRepository;
     }
+
     #[Route('/dashboard', name: 'app_dashboard_index')]
-    public function index(EntityManagerInterface $entityManager, ClientsRepository $clientsRepository): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer l'utilisateur connecté
         $user = $this->getUser();
 
-        if (!$user instanceof User || !$user) {
+        // Rediriger si l'utilisateur n'est pas connecté
+        if (!$user instanceof User) {
             return $this->redirectToRoute('home');
         }
 
+        // Récupérer l'entreprise de l'utilisateur
         $entreprise = $user->getIdEntreprise();
+        $entityManager->initializeObject($entreprise);
 
+        // Calcul des statistiques
         $numberDevis = $entreprise->getDevis()->count();
-
         $numberClients = $entreprise->getClients()->count();
-
         $numberDevisAccept = $entreprise->getDevis()->filter(function($devis) {
             return $devis->getStatut() === 'Accepté';
         })->count();
-
         $numberFactures = $entreprise->getFactures()->count();
 
-
-
-        $entityManager->initializeObject($entreprise);
-
-        $totalClients = $clientsRepository->countClientsByEntreprise($entreprise);
-
+        // Récupérer le nombre total de clients
+        $totalClients = $this->clientsRepository->countClientsByEntreprise($entreprise);
 
         // Récupérer les trois derniers clients
         $recentClients = $this->clientsRepository->findLatestClients();
 
-        // $sumAllPaiement = $entreprise->getFactures()->map(function($facture) {
-        //     return $facture->getPaiements()->map(function($paiement) {
-        //         return $paiement->getMontant();
-        //     })->sum();
-        // });
-
+        // A remplacer par des calculs réels.
         $sumAllPaiement = 1000;
-
         $sumDevisAccept = 550;
-
         $turnOver = 1500;
 
-
-        // dd($numberDevisAccept);
-
-        // Je veux renvoyer : 
-        // Nombre de devis de l'entreprise
-        // Nombre de devis accepté 
-        // Nombre de factures 
-
-        // Somme des paiement recu
-        // Somme des devis accepté
-        // Chiffre d'affaire
-
+        // Renvoyer les données au template
         return $this->render('dashboard/index.html.twig', [
+            'user' => $user,
             'numberDevis' => $numberDevis,
             'numberClients' => $numberClients,
             'totalClients' => $totalClients,
@@ -81,7 +63,7 @@ class DashboardController extends AbstractController
             'numberFactures' => $numberFactures,
             'sumAllPaiement' => $sumAllPaiement,
             'sumDevisAccept' => $sumDevisAccept,
-            'turnOver' => $turnOver
+            'turnOver' => $turnOver,
         ]);
     }
 }
