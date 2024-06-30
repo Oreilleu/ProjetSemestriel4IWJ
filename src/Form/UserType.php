f983d1b7af4e4a075a7c15000e1e9fe99b1cfaef
Salim-T <tizi_salim@yahoo.fr>
2024-06-30 17:48:07 +0200

    add password constraint

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-use App\Entity\Entreprises;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -11,68 +10,65 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        if($options['onSettingsPage'] === false) {
-            $builder
-                ->add('email', EmailType::class, [
-                    'required' => true,
-                ])
-                ->add('roles', ChoiceType::class, [
-                    'choices' => [
-                        'Utilisateur' => 'ROLE_ENTREPRISE',
-                        'Comptable' => 'ROLE_COMPTABLE',
+        $builder
+            ->add('email', EmailType::class, [
+                'required' => true,
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_ENTREPRISE',
+                    'Comptable' => 'ROLE_COMPTABLE',
+                ],
+                'multiple' => false,
+                'expanded' => true,
+                'label' => 'Rôle',
+                'required' => true,
+            ])
+            ->add('id_entreprise')
+            ;
+            if ($options['data']->getId() === null) {
+                $builder->add('password', PasswordType::class, [
+                    'mapped' => false,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 12,
+                            'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+                        ]),
+                        new Regex(
+                            [
+                                'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/',
+                                'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial',
+                            ]
+                        )
                     ],
-                    'label' => 'Rôle',
-                    'required' => true,
-                ])
-                ->add('id_entreprise', ChoiceType::class, [
-                    'choices' => $options['id_entreprises'],
-                    'choice_label' => 'nom',
-                    'label' => 'Entreprise',
-                    'required' => true,
-                    'placeholder' => 'Choisir une entreprise',
                 ]);
-
-                if ($options['data']->getId() === null) {
-                    $builder->add('password', PasswordType::class, [
-                        'required' => true,
-                    ]);
+            }
+            $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    return [$rolesString];
                 }
-
-                $builder->get('roles')
-                ->addModelTransformer(new CallbackTransformer(
-                    function ($rolesArray) {
-                        return count($rolesArray) ? $rolesArray[0] : null;
-                    },
-                    function ($rolesString) {
-                        return [$rolesString];
-                    }
-                ));
-        } else {
-            $builder
-                ->add('theme_color', ChoiceType::class, [
-                    'choices' => [
-                        'Classique' => 'classique',
-                        'Dark' => 'dark',
-                        'Bleu' => 'blue',
-                    ],
-                    'label' => 'Thème',
-                    'required' => true,
-                    'mapped' => true,
-                ]);
-        }
-    } 
+            ));
+            }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
-            'onSettingsPage' => false,
-            'id_entreprises' => null,
         ]);
     }
 }
