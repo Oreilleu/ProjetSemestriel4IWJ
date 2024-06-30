146c53e7d0b9902411375ad80deaba2b427c2ebd
Salim-T <tizi_salim@yahoo.fr>
2024-06-30 15:42:00 +0200

    Fix file use in categoriesProduitsType

--- src/Form/CategoriesProduitsType.php
+++ src/Form/CategoriesProduitsType.php
@@ -3,11 +3,11 @@
 namespace App\Form;
 
 use App\Entity\CategoriesProduits;
-use FontLib\EOT\File;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class CategoriesProduitsType extends AbstractType
 {
